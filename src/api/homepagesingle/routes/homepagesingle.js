7f3c77f38010a1fb777724f56e97d9817c9a767f
AntonMir <toshik124@rambler.ru>
2022-05-08 23:29:16 +0300

    remake contacts collection

--- src/api/homepagesingle/routes/homepagesingle.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * homepagesingle router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::homepagesingle.homepagesingle');
--- src/api/homepagesingle/services/homepagesingle.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * homepagesingle service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::homepagesingle.homepagesingle');
