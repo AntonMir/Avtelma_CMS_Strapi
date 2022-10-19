1ea2009b9e45c6e75c17bc631c94bf24a5988f58
AntonMir <toshik124@rambler.ru>
2022-10-19 17:54:10 +0300

    change .env3

--- config/server.js
+++ config/server.js
@@ -2,7 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: ['YjXLqXr4za5hGeaxXYPFiw==', 'GhsGw87WDInw50ep80pqDA=='],
-    // keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS'),
   },
 });
