5ed882a09e423df7ef5f8b64ffe39023b60c10ac
AntonMir <toshik124@rambler.ru>
2022-10-19 17:58:53 +0300

    .env5

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS', ['YjXLqXr4za5hGeaxXYPFiw==','GhsGw87WDInw50ep80pqDA==','LznhkNwehTSdTgtQGrIwIQ==','rDeMD+YDySR5NqzRgXGXOg==']),
+    keys: ['YjXLqXr4za5hGeaxXYPFiw==','GhsGw87WDInw50ep80pqDA==','LznhkNwehTSdTgtQGrIwIQ==','rDeMD+YDySR5NqzRgXGXOg=='],
   },
 });
